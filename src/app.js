672668e9e61e3a5322d56fc532f408bbfb7c3a03
luigui52 <luigui52@gmail.com>
2021-06-19 22:02:58 +0000

    Project almost finished, pending randomBtn

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@ import "./styles.css";
 const reviews = [
   {
     id: 1,
-    name: "susan smith",
-    job: "web developer",
+    name: "Luigi Hidalgo",
+    job: "Full Stack web developer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+      "https://scontent.fsjo7-1.fna.fbcdn.net/v/t1.6435-9/158608401_10227409436640528_8683176417125136492_n.jpg?_nc_cat=109&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=hmS_Ek77dxoAX-XauUD&tn=HlCY3L_Fz5XMo9W-&_nc_ht=scontent.fsjo7-1.fna&oh=14460788c59afde94d8d3999b9a16a05&oe=60D3854B",
+    text: "I am a legal compliance analyst with experience in Code solutions"
   },
   {
     id: 2,
@@ -49,3 +48,51 @@ const info = document.getElementById("info");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
+
+//set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function() {
+  showPerson();
+  //   const item = reviews[currentItem];
+  //   img.src = item.img;
+  //   author.textContent = item.name;
+  //   job.textContent = item.job;
+  //   info.textContent = item.text;
+});
+
+//show person based on item
+
+function showPerson(person) {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener("click", function() {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// show previous person
+prevBtn.addEventListener("click", function() {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.lenght - 1;
+  }
+  showPerson();
+});
+
+//Challenge** Ramdom Person by clicking on "Surprise me" button
+randomBtn.addEventListener("click", function() {
+  currentItem = Math.floor(Math.random() * reviews.lenght);
+  showPerson();
+});
